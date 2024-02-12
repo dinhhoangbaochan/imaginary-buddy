b6135439347497f7cd8566e75bb5b389aae17411
Chan Dinh <dinhhoangbaochan@gmail.com>
2024-02-12 14:40:21 +0700

    add basic styles

--- commands.php
+++ commands.php
@@ -1,8 +1,21 @@
 <?php
 
 class Console {
-  
-  function __construct() {
 
+  private $styles = [
+    'default' => "\033[0m",
+    'red'     => "\033[0;31m",
+    'green'   => "\033[0;32m",
+    'yellow'  => "\033[0;33m",
+    'blue'    => "\033[0;34m",
+    'magenta' => "\033[0;35m",
+    'cyan'    => "\033[0;36m",
+    'bold'    => "\033[1m",
+  ];
+
+  public function write( string $message, string $color )
+  {
+    echo $this->styles[$color] . $message;
   }
+
 }
